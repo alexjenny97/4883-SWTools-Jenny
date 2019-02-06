9e368db75478d161aaf22b3be474896cd1776be0
alexjenny97 <alexjenny97@gmail.com>
2019-02-05 23:55:43 -0600

    better comments/clean up

--- Assignments/A03/ScrapingNFL.py
+++ Assignments/A03/ScrapingNFL.py
@@ -1,56 +1,56 @@
+# Alex Jenny
+# Scrapes NFL websites to get json files with all stats
+import json
+import time
 from beautifulscraper import BeautifulScraper
 from validator_collection import checkers
-import os,sys
-import json
-import pprint as pp
-import time
-Data = {}
 
-gameIds = []
-scraper = BeautifulScraper()
-# fix later to get all data
-years = [x for x in range(2009, 2019)]
-stype = "REG"
-weeks = [x for x in range(1,18)]
+
+DATA = {}
+GAME_IDS = []
+SCRAPER = BeautifulScraper()
+YEARS = [x for x in range(2009, 2019)] # 2009 - 2018
+S_TYPE = "REG"
+WEEKS = [x for x in range(1, 18)] # 17 REG games per year
 
 
 
 # get game ids for regular!!
-for yr in years:
-    for wk in weeks:
-        time.sleep(2)
-        url = "http://www.nfl.com/schedules/%d/%s%s" % (yr,stype,str(wk))
-        if(checkers.is_url(url)):
-            page = scraper.go(url)
-            divs = page.find_all("div", {"class" : "schedules-list-content"})
+for yr in YEARS:
+    for wk in WEEKS:
+        time.sleep(.2) # good practice to not be too fast with requests
+        # Since post games are handeled S_TYPE could be hard-coded but this also works 
+        url = "http://www.nfl.com/schedules/%d/%s%s" % (yr, S_TYPE, str(wk))
+        if checkers.is_url(url): # then url has data
+            page = SCRAPER.go(url) 
+            divs = page.find_all("div", {"class" : "schedules-list-content"}) # div contains gameid
 
             for d in divs:
-                gameIds.append(d["data-gameid"])
+                GAME_IDS.append(d["data-gameid"]) #all regular game ids are being  stored
 
 
 
 #get game ids for post!
-stype = "POST"
+S_TYPE = "POST"
 
-for yr in years:
+for yr in YEARS: # now week number for post season games
     time.sleep(.2)
-    url = "http://www.nfl.com/schedules/%d/%s" % (yr,stype)
-    if(checkers.is_url(url)):
-        page = scraper.go(url)
+    url = "http://www.nfl.com/schedules/%d/%s" % (yr, S_TYPE) 
+    if checkers.is_url(url):
+        page = SCRAPER.go(url)
         divs = page.find_all("div", {"class" : "schedules-list-content"})
 
         for d in divs:
-            gameIds.append(d["data-gameid"])
+            GAME_IDS.append(d["data-gameid"])
 
 
-
-
-#for all game ids
-for g in gameIds:
+# get actual data for all game ids
+for g in GAME_IDS:
     time.sleep(.2)
+    # this url requires a game id
     url = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (g,g)
-    if(checkers.is_url(url)):
-        page = scraper.go(url)
+    if checkers.is_url(url):
+        page = SCRAPER.go(url)
+        # put data into a json file. about 2500
         json.dump(page.get_text(), open("NFLData/NFLData%s.json" % (g), 'w'))
-
-
+        
